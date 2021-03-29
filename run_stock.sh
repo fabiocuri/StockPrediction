f889bb1feed872cfacbcddd6e9a15f0cb6e48565
fabiocuri <fcuri91@gmail.com>
2021-03-29 13:25:34 +0100

    Update run_stock.sh

--- run_stock.sh
+++ run_stock.sh
@@ -3,13 +3,9 @@ HPT="NO"
 if [[ $(date +%u) -gt 5 ]]; then
  HPT="YES"
 fi
-#FOLDER="/home/fabiocuri/Desktop/stockprediction"
 FOLDER="/home/rajaramiimb/backend"
 PARAMETERS="$FOLDER/parameters_default.json"
 STOCKS="$FOLDER/sp500.txt"
-#virtualenv stockenv
-#source stockenv/bin/activate
-#pip install -r requirements.txt
 while IFS= read -r line
 do
   content=$line
@@ -17,5 +13,4 @@ do
   python3.7 $FOLDER/main.py $stock $PARAMETERS $HPT
 done < "$STOCKS"
 python3.7 $FOLDER/generate_reports.py $PARAMETERS
-#python $FOLDER/accuracy.py $PARAMETERS
 deactivate
